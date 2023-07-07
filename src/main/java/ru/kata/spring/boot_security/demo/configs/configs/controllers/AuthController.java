5c48dff15ca0a2a6c8f9b6f0cd591511fed54427
Kiril Vasilyeu <cool.kirill.vasiljev.01@yandex.ru>
2023-07-07 14:53:55 +0300

    Finish Commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/configs/controllers/AuthController.java
@@ -0,0 +1,14 @@
+package ru.kata.spring.boot_security.demo.configs.configs.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/")
+    public String getAuthenticated() {
+        return "index";
+    }
+
+}
